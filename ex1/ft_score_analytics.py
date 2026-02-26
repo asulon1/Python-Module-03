54aaf0149aad481c60a69aed164eea8f4a0bca92
asulon <adrien.sulon@epitech.eu>
2026-02-26 06:56:33 +0100

    add ex1

---
+++ ex1/ft_score_analytics.py
@@ -0,0 +1,35 @@
+# ************************************************************************* #
+#                                                                           #
+#                                                      :::      ::::::::    #
+#  ft_score_analytics.py                             :+:      :+:    :+:    #
+#                                                  +:+ +:+         +:+      #
+#  By: asulon <asulon@student.42nice.fr>         +#+  +:+       +#+         #
+#                                              +#+#+#+#+#+   +#+            #
+#  Created: 2026/02/26 06:29:40 by asulon          #+#    #+#               #
+#  Updated: 2026/02/26 06:55:38 by asulon          ###   ########.fr        #
+#                                                                           #
+# ************************************************************************* #
+
+import sys
+
+if len(sys.argv) == 1:
+    print("No score provided."
+          "Usage: python3 ft_score_analytics.py <score1> <score2> ...")
+else:
+    print("=== Player Score Analytics ===")
+    score_list = []
+
+    i = 1
+    while i < len(sys.argv):
+        try:
+            score_list.append(int(sys.argv[i]))
+        except ValueError:
+            print(f"oops, I typed '{sys.argv[i]}' instead of '1000'")
+        i += 1
+    print(f"Scores processed: {score_list}")
+    print(f"Total players: {len(score_list)}")
+    print(f"Total score: {sum(score_list)}")
+    print(f"Average score: {sum(score_list) / len(score_list)}")
+    print(f"High score: {max(score_list)}")
+    print(f"Low score: {min(score_list)}")
+    print(f"Score range: {max(score_list) - min(score_list)}")
