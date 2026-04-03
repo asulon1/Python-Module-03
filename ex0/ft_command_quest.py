b654e4e3618188756f8c809801db09ebe35a1d93
asulon <adrien.sulon@gmail.com>
2026-04-03 21:33:27 +0200

    ex0/ex1 update with new subject

--- ex0/ft_command_quest.py
+++ ex0/ft_command_quest.py
@@ -13,10 +13,8 @@
 import sys
 
 print("=== Command Quest ===")
-if len(sys.argv) == 1:
-    print("No arguments provided!")
 
-print(f"Program name: {sys.argv[0]}")
+print(f"Program name: {sys.argv[0].split('/')[1]}")
 if len(sys.argv) > 1:
     print(f"Arguments received: {len(sys.argv) - 1}")
     i = 1
@@ -24,5 +22,7 @@ if len(sys.argv) > 1:
         print(f"Argument {i}: ", end="")
         print(sys.argv[i])
         i += 1
+else:
+    print("No arguments provided!")
 
 print(f"Total arguments: {len(sys.argv)}")
--- ex1/ft_score_analytics.py
+++ ex1/ft_score_analytics.py
@@ -13,19 +13,17 @@
 import sys
 
 print("=== Player Score Analytics ===")
-if len(sys.argv) == 1:
-    print("No score provided."
-          "Usage: python3 ft_score_analytics.py <score1> <score2> ...")
-else:
-    score_list = []
+score_list = []
 
-    i = 1
-    while i < len(sys.argv):
-        try:
-            score_list.append(int(sys.argv[i]))
-        except ValueError:
-            print(f"oops, I typed '{sys.argv[i]}' instead of '1000'")
-        i += 1
+i = 1
+while i < len(sys.argv):
+    try:
+        score_list.append(int(sys.argv[i]))
+    except ValueError:
+        print(f"Invalid parameter '{sys.argv[i]}'")
+    i += 1
+
+if len(score_list) > 0:
     print(f"Scores processed: {score_list}")
     print(f"Total players: {len(score_list)}")
     print(f"Total score: {sum(score_list)}")
@@ -33,3 +31,6 @@ else:
     print(f"High score: {max(score_list)}")
     print(f"Low score: {min(score_list)}")
     print(f"Score range: {max(score_list) - min(score_list)}")
+else:
+    print("No score provided. "
+          "Usage: python3 ft_score_analytics.py <score1> <score2> ...")
